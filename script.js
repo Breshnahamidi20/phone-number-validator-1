bb0ab54804bc2f9e48a8ec7e1daa05058e0e48ec
Breshnahamid20 <breshna2004@gmail.com>
2024-07-11 08:09:35 +0430

    add script file

---
+++ script.js
@@ -0,0 +1,31 @@
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
+
+checkBtn.addEventListener('click', function() {
+  const userInputValue = userInput.value.trim();
+  
+  if (userInputValue === '') {
+    alert('Please provide a phone number');
+    return;
+  }
+  const validPatterns = [
+   /^1?\s?\d{3}-\d{3}-\d{4}$/,
+   /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
+   /^1?\(\d{3}\)\d{3}-\d{4}/,
+   /^1?\s?\d{3}\s?\d{3}\s?\d{4}$/,
+   /^1?\s?\d{10}$/
+];
+
+  
+  const isValid = validPatterns.some((pattern) => pattern.test(userInputValue));
+  resultsDiv.textContent = isValid ? `Valid US number: ${userInputValue}` : `Invalid US number: ${userInputValue}`;
+  resultsDiv.style.color = isValid ? 'green' : 'red';
+});
+
+clearBtn.addEventListener('click', function() {
+  resultsDiv.textContent = '';
+  userInput.value = '';
+});
+
