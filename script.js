05f2d269385c288f7779a7f4cd26e737b13d2aff
Breshnahamid20 <breshna2004@gmail.com>
2024-07-11 08:33:43 +0430

    solve linters error

--- script.js
+++ script.js
@@ -3,29 +3,24 @@ const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const resultsDiv = document.getElementById('results-div');
 
-checkBtn.addEventListener('click', function() {
+checkBtn.addEventListener('click', () => {
   const userInputValue = userInput.value.trim();
-  
   if (userInputValue === '') {
-    alert('Please provide a phone number');
+    window.alert('Please provide a phone number');
     return;
   }
   const validPatterns = [
-   /^1?\s?\d{3}-\d{3}-\d{4}$/,
-   /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
-   /^1?\(\d{3}\)\d{3}-\d{4}/,
-   /^1?\s?\d{3}\s?\d{3}\s?\d{4}$/,
-   /^1?\s?\d{10}$/
-];
-
-  
+    /^1?\s?\d{3}-\d{3}-\d{4}$/,
+    /^1?\s?\(\d{3}\)\s?\d{3}-\d{4}$/,
+    /^1?\(\d{3}\)\d{3}-\d{4}/,
+    /^1?\s?\d{3}\s?\d{3}\s?\d{4}$/,
+    /^1?\s?\d{10}$/,
+  ];
   const isValid = validPatterns.some((pattern) => pattern.test(userInputValue));
   resultsDiv.textContent = isValid ? `Valid US number: ${userInputValue}` : `Invalid US number: ${userInputValue}`;
   resultsDiv.style.color = isValid ? 'green' : 'red';
 });
-
-clearBtn.addEventListener('click', function() {
+clearBtn.addEventListener('click', () => {
   resultsDiv.textContent = '';
   userInput.value = '';
 });
-
